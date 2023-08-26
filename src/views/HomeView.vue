79f2dd3cabcd96193c47ab9f9e9da54b323c4ca9
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-26 11:40:44 -0400

    Created landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <!-- <div class="home"> -->
+  <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <!-- </div> -->
+  <section class="home-landing">
+    <div class="container">
+      <h2>Welcome to Your Local Parks!</h2>
+      <p>
+        Here you can check out the status of your parks and see what's available
+        at those parks.
+      </p>
+      <p>
+        You can also see what amazing events we have planned at our local parks.
+        Events are fun for all ages.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
