83c47feea89618e6a537508fe600a75a570fcdf4
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-27 19:36:17 -0400

    Added condition for authentication

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,14 +1,28 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- Do this instead of router guard -->
+    <router-link :class="{ disabled: !userStore.isAuthenticated }" to="/about"
+      >About</router-link
+    >
   </nav>
 </template>
 
 <script>
+import { useUserStore } from "../store/userStore";
+
 export default {
   name: "HeaderNav",
+  setup() {
+    const userStore = useUserStore();
+
+    return { userStore };
+  },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.disabled {
+  display: none;
+}
+</style>
