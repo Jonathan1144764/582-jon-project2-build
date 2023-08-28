3d51c168cc44f411600a8a711d691425b22cbba7
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-28 16:09:47 -0400

    Added park facilities input component

---
+++ src/components/ParkFacilitiesInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <label for="soccer">Soccer fields: </label>
+  <input type="number" name="soccer" id="soccer" />
+  <label for="baseball">Baseball diamonds: </label>
+  <input type="number" name="baseball" id="baseball" />
+  <label for="bathrooms">Bathrooms: </label>
+  <input type="number" name="bathrooms" id="bathrooms" />
+  <label for="playground">Playgrounds: </label>
+  <input type="number" name="playground" id="playground" />
+</template>
+
+<script>
+export default {
+  name: "ParkFacilitiesInput",
+};
+</script>
--- src/components/ParkForm.vue
+++ src/components/ParkForm.vue
@@ -1,19 +1,26 @@
 <template>
   <form>
     <ParkNameInput />
-    <ParkStatusInput />
+    <div class="park-radio-inputs">
+      <ParkStatusInput />
+    </div>
+    <div class="park-number-inputs">
+      <ParkFacilitiesInput />
+    </div>
   </form>
 </template>
 
 <script>
 import ParkNameInput from "./ParkNameInput.vue";
 import ParkStatusInput from "./ParkStatusInput.vue";
+import ParkFacilitiesInput from "./ParkFacilitiesInput.vue";
 
 export default {
   name: "ParkFrom",
   components: {
     ParkNameInput,
     ParkStatusInput,
+    ParkFacilitiesInput,
   },
 };
 </script>
