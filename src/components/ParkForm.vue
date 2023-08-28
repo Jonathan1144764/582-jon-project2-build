b50a69da757f2ae2c2ff4ce6dea3245cdcac6c45
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-28 12:38:26 -0400

    Added status input for park form

--- src/components/ParkForm.vue
+++ src/components/ParkForm.vue
@@ -1,16 +1,19 @@
 <template>
   <form>
     <ParkNameInput />
+    <ParkStatusInput />
   </form>
 </template>
 
 <script>
 import ParkNameInput from "./ParkNameInput.vue";
+import ParkStatusInput from "./ParkStatusInput.vue";
 
 export default {
   name: "ParkFrom",
   components: {
     ParkNameInput,
+    ParkStatusInput,
   },
 };
 </script>
---
+++ src/components/ParkStatusInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <input type="radio" name="status" value="open" id="park-status-open" />
+  <label for="park-status-open">Open</label>
+  <input type="radio" name="status" value="closed" id="park-status-closed" />
+  <label for="park-status-closed">Closed</label>
+  <input
+    type="radio"
+    name="status"
+    value="maintenance"
+    id="park-status-maintenance"
+  />
+  <label for="park-status-maintenance">Closed for maintenance</label>
+  <input
+    type="radio"
+    name="status"
+    value="construction"
+    id="park-status-construction"
+  />
+  <label for="park-status-construction">Under construction</label>
+</template>
+
+<script>
+export default {
+  name: "ParkStatusInput",
+};
+</script>
