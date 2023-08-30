e3b02239e71f3513982852aa6fadd615bdb614bc
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-30 15:59:20 -0400

    Added event start date input component

---
+++ src/components/EventStartDateInput.vue
@@ -0,0 +1,10 @@
+<template>
+    <label for="start-date">Start Date: </label>
+    <input type="date" name="start-date" id="start-date-input">
+</template>
+
+<script>
+export default {
+    name: "EventStartDateInput",
+}
+</script>
