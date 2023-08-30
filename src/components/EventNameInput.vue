ce5e871d832ca571d1e75a742adaf95383ac8bdb
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-30 15:54:06 -0400

    Added event name input component

---
+++ src/components/EventNameInput.vue
@@ -0,0 +1,10 @@
+<template>
+  <label for="name">Event name: </label>
+  <input type="text" name="name" id="event-name-input" />
+</template>
+
+<script>
+export default {
+  name: "EventnameInput",
+};
+</script>
