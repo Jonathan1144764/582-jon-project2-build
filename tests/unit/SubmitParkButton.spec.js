f39ede46b38dca575e8042626c278170a787f2dc
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-30 14:11:55 -0400

    Added file for park buttons test

---
+++ tests/unit/SubmitParkButton.spec.js
@@ -0,0 +1,8 @@
+// import { shallowMount } from "@vue/test-utils";
+// import SubmitParkButton from "@/components/SubmitParkButton.vue";
+
+// describe("SubmitParkButton", () => {
+//     it("displays submit button when route contains /newpark", () => {
+
+//   });
+// });
--- tests/unit/SubmitParkForm.spec.js
+++
@@ -1,5 +0,0 @@
-// import { shallowMount } from "@vue/test-utils";
-
-// describe("SubmitParkForm", () => {
-//   it("emits an event to add new entry to data", () => {});
-// });
