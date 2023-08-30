56215c55bb74e3e843ee9c43a1577255f1a6faac
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-30 13:31:26 -0400

    Edited test

--- tests/unit/CreateUsers.spec.js
+++ tests/unit/CreateUsers.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import CreateUsers from "@/components/CreateUsers.vue";
 
 describe("CreateUsers.vue", () => {
-  it("creates a new user when clicked", async () => {
+  it("creates a new user when clicked and emits an event with the new user id", async () => {
     const wrapper = shallowMount(CreateUsers);
     await wrapper.find("button").trigger("click");
     expect(wrapper.emitted().refetchUsers[0][0]).toBe(1);
