caa90ab09ff0bad57e9d6c3a1ac154453d785127
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-31 17:00:53 -0400

    Created Event description input component

---
+++ src/components/EventDescriptionInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <label for="description">Description: </label>
+  <textarea
+    name="descriptoin"
+    id="event-description-input"
+    cols="30"
+    rows="10"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: "EventDescriptionInput",
+};
+</script>
--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,18 +2,21 @@
   <form>
     <EventNameInput />
     <EventDatesInputs />
+    <EventDescriptionInput />
   </form>
 </template>
 
 <script>
 import EventNameInput from "./EventNameInput.vue";
 import EventDatesInputs from "./EventDatesInputs.vue";
+import EventDescriptionInput from "./EventDescriptionInput.vue";
 
 export default {
   name: "EventForm",
   components: {
     EventNameInput,
     EventDatesInputs,
+    EventDescriptionInput,
   },
 };
 </script>
