a17a36480bca8807e6b91e4e37699bd4626ee79d
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-31 10:01:30 -0400

    Created event form component

---
+++ src/components/EventForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <form>
+        <EventNameInput />
+        <EventDatesInputs />
+    </form>
+</template>
+
+<script>
+import EventNameInput from "./EventNameInput.vue";
+import EventDatesInputs from "./EventDatesInputs.vue";
+
+export default {
+    name: "EventForm",
+    components: {
+        EventNameInput,
+        EventDatesInputs
+    }
+}
+</script>
