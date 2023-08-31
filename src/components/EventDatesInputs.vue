bd652073ba5a1f3ede1f44e1477f40b88c5bbffd
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-31 09:58:18 -0400

    Added container component for date inputs

---
+++ src/components/EventDatesInputs.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="event-dates-inputs">
+    <EventStartDateInput />
+    <EventEndDateInput />
+  </div>
+</template>
+
+<script>
+import EventStartDateInput from "./EventStartDateInput.vue";
+import EventEndDateInput from "./EventEndDateInput.vue";
+
+export default {
+  name: "EventDatesInputs",
+  components: {
+    EventStartDateInput,
+    EventEndDateInput,
+  },
+};
+</script>
