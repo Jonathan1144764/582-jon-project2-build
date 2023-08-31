40aeab8fcafdc8d85ee3bdd5c2926eea1a258cb6
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-31 10:06:15 -0400

    Created view file for creating new events

---
+++ src/views/NewEventFormView.vue
@@ -0,0 +1,14 @@
+<template>
+    <EventForm />
+</template>
+
+<script>
+import EventForm from "../components/EventForm.vue";
+
+export default {
+    name: "NewEventFormView",
+    components: {
+        EventForm,
+    }
+}
+</script>
