c692cfe8a2f0658a85e0f75deda102f5d9726832
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-02 16:02:01 -0400

    Created test file for event select

---
+++ tests/unit/EventSelect.spec.js
@@ -0,0 +1,10 @@
+// import { shallowMount } from "@vue/test-utils";
+// import EventSelect from "@/components/EventSelect.vue";
+
+// describe("EventSelect", () => {
+//   it("emits an event to populate form on value change", async () => {
+//     const wrapper = shallowMount(EventSelect);
+//     await wrapper.vm.selectEvent();
+//     console.log(wrapper.emitted());
+//   });
+// });
