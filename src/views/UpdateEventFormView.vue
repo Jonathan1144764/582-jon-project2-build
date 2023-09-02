7536f67fc5290f6e301d50673b959c7a2b469586
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-01 20:51:29 -0400

    Added fetch function

--- src/views/UpdateEventFormView.vue
+++ src/views/UpdateEventFormView.vue
@@ -1,5 +1,5 @@
 <template>
-  <EventSelect />
+  <EventSelect :allEvents="allEvents" />
 </template>
 
 <script>
@@ -7,8 +7,31 @@ import EventSelect from "../components/EventSelect.vue";
 
 export default {
   name: "UpdateEventFormView",
+  data() {
+    return {
+      allEvents: [],
+      selectedEvent: {},
+    };
+  },
   components: {
     EventSelect,
+  },
+  methods: {
+    fetchEvents() {
+      this.allEvents = [];
+      fetch(
+        "https://special-doodle-r949xwgp9jpf5w56-3000.app.github.dev/admin/updateevent"
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          for (let event of json) {
+            this.allEvents.push(event);
+          }
+        });
+    },
+  },
+  created() {
+    this.fetchEvents();
   },
 };
 </script>
