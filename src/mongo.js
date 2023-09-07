a3453f3c0d198bec56f08de3d57e04b353725f39
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-07 17:11:31 -0400

    Added function to serve static files

--- src/mongo.js
+++ src/mongo.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("582-jon-project2-build/public"));
+
 var cors = require("cors");
 app.use(cors());
 
